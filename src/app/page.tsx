f785d18a44b2e69f98e4a1ffbbfd75f94117b985
Dragan <Dragan>
2024-10-26 01:12:45 +0200

    qq

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import Root from "./components/root.jsx";
-
-export default function Home() {
-  return <Root store={{}} />;
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import Root from "../components/root.jsx";
+
+export default function Home() {
+  return <div>fffffffffffffffff</div>;
+  //return <Root store={{}} />;
+}
