f4d35b10dac54743fa74307c44f26e9396da0a10
Dragan <Dragan>
2024-10-26 20:08:49 +0200

    qq

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -8,73 +8,64 @@ import { fetchAllUsers } from '../../actions/users_actions';
 import { fetchCartItems } from '../../actions/cart_items_actions';
 
 
-class Navbar extends React.Component{
+export default class Navbar extends React.Component{
     componentDidMount(){
         this.props.fetchAllUsers();
     }
     
     render() {
 
-        let {navbar, shopId} = this.props;
-        const development = (
-            <ul className="category-ul">
-                <li><Link to={`/categories/56`}>Jewelry & Accessories</Link></li>
-                <li><Link to={`/categories/57`}>Clothing & Shoes</Link></li>
-                <li><Link to={`/categories/58`}>Home & Living</Link></li>
-                <li><Link to={`/categories/59`}>Wedding & Party</Link></li>
-                <li><Link to={`/categories/60`}>Toys & Entertainment</Link></li>
-                <li><Link to={`/categories/61`}>Art & Collectibles</Link></li>
-                <li><Link to={`/categories/62`}>Craft Supplies & Tools</Link></li>
-                <li><Link to={`/categories/63`}>Vintage</Link></li>
-            </ul>
-        );
-
-        const production = (
-            <ul className="category-ul">
-                <li><Link to={`/categories/1`}>Jewelry & Accessories</Link></li>
-                    <li><Link to={`/categories/2`}>Clothing & Shoes</Link></li>
-                    <li><Link to={`/categories/3`}>Home & Living</Link></li>
-                    <li><Link to={`/categories/4`}>Wedding & Party</Link></li>
-                    <li><Link to={`/categories/5`}>Toys & Entertainment</Link></li>
-                    <li><Link to={`/categories/6`}>Art & Collectibles</Link></li>
-                    <li><Link to={`/categories/7`}>Craft Supplies & Tools</Link></li>
-                    <li><Link to={`/categories/8`}>Vintage</Link></li>
-
-            </ul>
-        );
-
-        const component = !navbar ? <LoggedOutNavbar/> : <LoggedInNavbar shopId={shopId} />;
         return (
-            <div className="navbar">
-                {component}
-                {/* {development} */}
-                {production}
+    <nav class="bg-gray-800">
+        <div class="container flex">
+            <div class="px-8 py-4 bg-primary md:flex items-center cursor-pointer relative group hidden">
+                <span class="text-white">
+                    <i class="fa-solid fa-bars"></i>
+                </span>
+                <span class="capitalize ml-2 text-white hidden">All Categories</span>
+
+                <div
+                    class="absolute w-full left-0 top-full bg-white shadow-md py-3 divide-y divide-gray-300 divide-dashed opacity-0 group-hover:opacity-100 transition duration-300 invisible group-hover:visible">
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/sofa.svg" alt="sofa" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">Sofa</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/terrace.svg" alt="terrace" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">Terarce</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/bed.svg" alt="bed" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">Bed</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/office.svg" alt="office" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">office</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/outdoor-cafe.svg" alt="outdoor" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">Outdoor</span>
+                    </a>
+                    <a href="#" class="flex items-center px-6 py-3 hover:bg-gray-100 transition">
+                        <img src="assets/images/icons/bed-2.svg" alt="Mattress" class="w-5 h-5 object-contain"/>
+                        <span class="ml-6 text-gray-600 text-sm">Mattress</span>
+                    </a>
+                </div>
             </div>
-        );
+
+            <div class="flex items-center justify-between flex-grow md:pl-12 py-5">
+                <div class="flex items-center space-x-6 capitalize">
+                    <a href="index.html" class="text-gray-200 hover:text-white transition">Home</a>
+                    <a href="pages/shop.html" class="text-gray-200 hover:text-white transition">Shop</a>
+                    <a href="#" class="text-gray-200 hover:text-white transition">About us</a>
+                    <a href="#" class="text-gray-200 hover:text-white transition">Contact us</a>
+                </div>
+                <a href="pages/login.html" class="text-gray-200 hover:text-white transition">Login</a>
+            </div>
+        </div>
+    </nav>)
     };
 } 
     
 
-const mapStateToProps = state => {
-    let shopId;
-    if (state.session.id){
-        shopId = currentUserHasShop(state.session.id, state.entities.users)
-    } else {
-        shopId = false;
-    };
 
-    return {
-        navbar: Boolean(state.session.id),
-        shopId,
-        users: selectAllUsers(state.entities.users),
-    }
-  
-};
-
-const mapDispatchToProps = dispatch => ({
-    fetchAllUsers: () => dispatch(fetchAllUsers()),
-    fetchCartItems: () => dispatch(fetchCartItems())
-})
-
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Navbar));
